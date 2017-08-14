8e56ae673dc770acb3cb52e694d388df89efa11c
Sasa Tatar <sasa.tatar@gmail.com>
2017-08-14 17:01:01 +0200

    FB config

---
+++ app/config/firebase-template.js
@@ -0,0 +1,9 @@
+// https://console.firebase.google.com/u/1/project/home-budget-d39d7/settings/general/
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
