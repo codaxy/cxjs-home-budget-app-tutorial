79236ad42ff061ea2d6f9c375ad41d53960c8773
Sasha Tatar <sasa.tatar@gmail.com>
2017-08-17 02:59:50 +0200

    ...

--- app/routes/sign-in/index.js
+++ app/routes/sign-in/index.js
@@ -14,11 +14,6 @@ export default () => <cx>
 					<img src="~/assets/sign-in/google/btn_google_signin_dark_normal_web.png" />
 				</a>
 			</p>
-			<p>
-				<a href="#" onClick="signInWithTwitter">
-					<img src="~/assets/sign-in/twitter/sign-in-with-twitter-gray.png" />
-				</a>
-			</p>
 		</Section>
 
 		<Section title="User Info" visible={expr("!!{user.id}")} ws>
